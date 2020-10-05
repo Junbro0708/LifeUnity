39e362385d040b1b36c123cb8df114cf697a910f
Junbro0708 <qkrwns708@soongsil.ac.kr>
2020-10-05 22:45:50 +0900

    I studied enemy cycle in Unity with c#

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    float currentTime;
+
+    public float createTime = 1;
+    public GameObject enemyFactory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if(currentTime > createTime)
+        {
+            GameObject enemy = Instantiate(enemyFactory);
+            enemy.transform.position = transform.position;
+            currentTime = 0;
+        }
+    }
+}
